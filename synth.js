8a78e74e947862a2b547a962ce9d423fc26a1004
Mike Dominice <mike@mikedominice.com>
2013-01-02 18:32:20 -0500

    initial work with ADSR envelope

--- synth.js
+++ synth.js
@@ -4,7 +4,7 @@
  * jquery.midikeys.js is freely distributable under the MIT license.
  */
 (function (root, factory) {
-    var libaryName = 'Synth';
+    var libaryName = 'synth';
     if (typeof define !== 'undefined' && define.amd) {
         define([], function () {
             return root[libaryName] = factory();
@@ -23,11 +23,66 @@
         return 440 * Math.pow(2, (note - 69) / 12.0);
     }
 
-    function Oscillator(context, note, velocity) {
+    function extend(result) {
+        var key, i;
+        for (i = 1; i < arguments.length; i++) {
+            for (key in arguments[i]) {
+                if (arguments[i].hasOwnProperty(key)) {
+                    result[key] = arguments[i][key];
+                }
+            }
+        }
+        return result;
+    }
+
+    function ADSREnvelope(context, options) {
+        options = extend({}, ADSREnvelope.defaults, options);
+
+        this.noteOn = function (param) {
+            param.cancelScheduledValues(context.currentTime - 0.1);
+            if (options.attack === 0) {
+                param.setValueAtTime(options.max, context.currentTime);
+            } else {
+                param.setValueAtTime(options.min, context.currentTime);
+                param.linearRampToValueAtTime(options.max, context.currentTime + options.attack);
+            }
+
+            if (options.decay !== 0) {
+                param.linearRampToValueAtTime(options.sustain * options.max, context.currentTime + options.attack + options.decay);
+            }
+        };
+
+        this.noteOff = function (param) {
+            param.cancelScheduledValues(context.currentTime - 0.1);
+            if (options.release === 0) {
+                param.setValueAtTime(options.min, context.currentTime);
+            } else {
+                param.linearRampToValueAtTime(options.min, context.currentTime + options.release);
+            }
+        };
+    }
+
+    ADSREnvelope.defaults = {
+        max : 1.0,
+        min : 0.0,
+        attack : 0,
+        decay : 0,
+        sustain : 1.0,
+        release : 0
+    };
+
+    function Oscillator(context, note, options) {
+        var opts = extend({}, Oscillator.defaults, options);
+        var maxGain = opts.velocity / MAX_VELOCITY;
+        
+        var volumeEnvelopeOptions = extend({}, opts.volumeEnvelope, {
+            max : maxGain
+        });
+        
         var gain = context.createGainNode();
-        gain.gain.value = velocity / MAX_VELOCITY;
-        console.log(gain.gain.value, velocity);
         var oscillator = context.createOscillator();
+        
+        var volumeEnvelope = new ADSREnvelope(context, volumeEnvelopeOptions);
         oscillator.frequency.value = midiNoteToFrequency(note);
 
         oscillator.connect(gain);
@@ -41,30 +96,43 @@
         };
 
         this.noteOn = function () {
+            volumeEnvelope.noteOn(gain.gain);
             return oscillator.noteOn.apply(oscillator, arguments);
         };
 
         this.noteOff = function () {
-            return oscillator.noteOff.apply(oscillator, arguments);
+            volumeEnvelope.noteOff(gain.gain);
+            // oscillator.noteOff(context.currentTime + (volumeEnvelopeOptions.release || 0));
         };
 
     }
 
-    function Synth(context) {
+    Oscillator.defaults = {
+        velocity : MAX_VELOCITY,
+        volumeEnvelope : extend({}, ADSREnvelope.defaults)
+    };
+
+    function Synth(context, opts) {
+        var options = extend({}, Synth.defaults, opts);
         var gain = context.createGainNode();
 
         var oscillators = {};
 
         function createOscillator(note) {
             var oscillator = context.createOscillator();
+            oscillator.type = options.type;
             oscillator.frequency.value = midiNoteToFrequency(note);
+            oscillator.type = options.type;
             return oscillator;
         }
         
         function noteOn(note, velocity) {
             var key = KEY_PREFIX + note;
             if (!oscillators.hasOwnProperty(key)) {
-                var oscillator = oscillators[key] = new Oscillator(context, note, velocity);
+                var oscillator = oscillators[key] = new Oscillator(context, note, {
+                    velocity : velocity,
+                    volumeEnvelope : options.volumeEnvelope
+                });
                 oscillator.connect(gain);
                 oscillator.noteOn(0);
             }
@@ -74,12 +142,11 @@
             var key = KEY_PREFIX + note;
             if (oscillators.hasOwnProperty(key)) {
                 var oscillator = oscillators[key];
-                oscillator.noteOff(0);
-                oscillator.disconnect(0);
+                oscillator.noteOff();
+                //oscillator.disconnect(0);
                 delete oscillators[key];
             }
         }
-
 
         this.send = function (data, timestamp) {
             var status = (data[0] & 0xf0);
@@ -98,6 +165,19 @@
         };
     }
 
-    return Synth;
+    extend(Synth, {
+        SINE : 0,
+        SQUARE : 1,
+        SAWTOOTH : 2,
+        TRIANGLE : 3
+    });
+
+    Synth.defaults = {
+        type : Synth.SINE
+    };
+
+    return {
+        Synth : Synth
+    };
 
 }));
